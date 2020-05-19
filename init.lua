67e2835a08cc2d09a4b66ff5f90b9184706b9b2d
Raitis Stengrevics <dagrevis@gmail.com>
2020-05-19 22:11:49 +0300

    Try out fzf as backend for filtering choices

--- init.lua
+++ init.lua
@@ -8,75 +8,10 @@ obj.author = 'Raitis Stengrevics'
 obj.homepage = 'https://github.com/daGrevis/PassChooser.spoon'
 obj.license = 'MIT - https://opensource.org/licenses/MIT'
 
--- http://lua-users.org/wiki/StringRecipes
-function string.ends(String, End)
-   return End == '' or string.sub(String, -string.len(End)) == End
-end
+local FZF_PATH = '/Users/dagrevis/.fzf/bin/fzf'
 
--- https://stackoverflow.com/a/15706820
-function spairs(t, order)
-    -- collect the keys
-    local keys = {}
-    for k in pairs(t) do keys[#keys+1] = k end
-
-    -- if order function given, sort by it by passing the table and keys a, b,
-    -- otherwise just sort the keys 
-    if order then
-        table.sort(keys, function(a,b) return order(t, a, b) end)
-    else
-        table.sort(keys)
-    end
-
-    -- return the iterator function
-    local i = 0
-    return function()
-        i = i + 1
-        if keys[i] then
-            return keys[i], t[keys[i]]
-        end
-    end
-end
-
--- https://gist.github.com/Badgerati/3261142
-function string.levenshtein(str1, str2)
-  local len1 = string.len(str1)
-  local len2 = string.len(str2)
-  local matrix = {}
-  local cost = 0
-
-  -- quick cut-offs to save time
-  if (len1 == 0) then
-    return len2
-  elseif (len2 == 0) then
-    return len1
-  elseif (str1 == str2) then
-    return 0
-  end
-
-  -- initialise the base matrix values
-  for i = 0, len1, 1 do
-    matrix[i] = {}
-    matrix[i][0] = i
-  end
-  for j = 0, len2, 1 do
-    matrix[0][j] = j
-  end
-
-  -- actual Levenshtein algorithm
-  for i = 1, len1, 1 do
-    for j = 1, len2, 1 do
-      if (str1:byte(i) == str2:byte(j)) then
-        cost = 0
-      else
-        cost = 1
-      end
-
-      matrix[i][j] = math.min(matrix[i-1][j] + 1, matrix[i][j-1] + 1, matrix[i-1][j-1] + cost)
-    end
-  end
-
-  -- return the last value - this is the Levenshtein distance
-  return matrix[len1][len2]
+local function endsWith(str, ending)
+   return ending == "" or str:sub(-#ending) == ending
 end
 
 local config = {
@@ -98,15 +33,19 @@ function obj:init(userConfig)
 end
 
 function obj:start()
-  local front_app = hs.application.frontmostApplication()
+  local frontApp = hs.application.frontmostApplication()
 
-  local output = hs.execute('find ' .. config.storePath .. ' -type f')
+  local findOutput = hs.execute('find ' .. config.storePath .. ' -type f')
 
-  local lines = hs.fnutils.filter(hs.fnutils.split(output, '\n'), function(line)
-    return line ~= '' and not line:ends('.gpg-id')
+  local findLines = hs.fnutils.split(findOutput, '\n')
+
+  findLines[#findLines] = nil
+
+  findLines = hs.fnutils.filter(findLines, function(line)
+    return not endsWith(line, '.gpg-id')
   end)
 
-  local all_texts = hs.fnutils.map(lines, function(line)
+  findLines = hs.fnutils.map(findLines, function(line)
     local filename = hs.fnutils.split(line, '//')[2]
     return filename:sub(0, filename:len() - 4)
   end)
@@ -123,7 +62,7 @@ function obj:start()
       end
     end
 
-    front_app:activate()
+    frontApp:activate()
   end
 
   local chooser = hs.chooser.new(function()
@@ -134,38 +73,19 @@ function obj:start()
   chooser:queryChangedCallback(function()
     local query = chooser:query()
 
-    local chars = {}
-    for i = 1, #query do
-      table.insert(chars, query:sub(i, i))
-    end
+    local fzfInput = table.concat(findLines, '\n')
 
-    -- http://lua-users.org/wiki/PatternsTutorial
-    local fuzzy_query = table.concat(chars, '.-')
+    local command = 'echo ' .. "'" .. fzfInput .. "'" .. ' | ' .. FZF_PATH .. ' -f ' .. "'" .. query .. "'"
 
-    local matching_texts = hs.fnutils.filter(all_texts, function(text)
-      return text:find(fuzzy_query)
-    end)
+    local fzfOutput = hs.execute(command)
 
-    local items = {}
-    for _, text in pairs(matching_texts) do
-      local distance
-      if query == '' then
-        distance = 1
-      else
-        distance = text:levenshtein(query)
-      end
-      table.insert(items, { text=text, distance=distance })
-    end
+    local fzfLines = hs.fnutils.split(fzfOutput, '\n')
+
+    fzfLines[#fzfLines] = nil
 
     choices = {}
-    for _, v in spairs(items, function(t, a, b)
-      if t[a].distance == t[b].distance then
-        return t[a].text < t[b].text
-      else
-        return t[a].distance < t[b].distance
-      end
-    end) do
-      table.insert(choices, { text=v.text })
+    for _, line in pairs(fzfLines) do
+      table.insert(choices, { text=line })
     end
 
     chooser:choices(choices)
